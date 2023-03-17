792ca09e31d01a7c8b2a4fbe3257e0b097197f4b
Jesus S <jsalazar2010@gmail.com>
2023-03-16 22:11:56 -0600

    npm install

---
+++ server.js
@@ -0,0 +1,17 @@
+// import Express and other NPM packages
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// API Routing and middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Listen for server on port 3001
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
