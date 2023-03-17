a72a61744c582857ce5cc0da843e5526200d90e3
Jesus S <jsalazar2010@gmail.com>
2023-03-16 22:18:48 -0600

    routing files setup

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // import Express and other NPM packages
 const express = require('express');
+// import Routing files
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// start Express app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
