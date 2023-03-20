4764acbcf5db97aa0df210301bf330d71e397b10
Jesus S <jsalazar2010@gmail.com>
2023-03-19 21:43:35 -0600

    fix routing files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,28 @@
 const router = require('express').Router();
+const store = require('./db/store');
 
 // GET method for reading notes
-router.get('', (req, res) => {
-    
-    .getNotes()
-    .then((notes) => {
-      return res.json(notes);
-    })
-    // catch error 404 response 
-    .catch ((err) => res.status(404).json(err));
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        // catch error 404 response 
+        .catch((err) => res.status(404).json(err));
 
 });
-
 // POST method for add notes
-.addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(404).json(err));
+router.post('/notes', (req, res) => {
+    store
+        .postNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(404).json(err));
 });
 
 //  DELETE notes function here 
-//DELETE /api/notes/:id
+//  router.delete('/api/notes/:id', { });
+
 
 // export router file
 module.exports = router;
