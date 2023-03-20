089f7b282491a0e8c9e42e0d33a60bdb12c17e84
Jesus S <jsalazar2010@gmail.com>
2023-03-19 21:15:18 -0600

    update readme file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,12 +5,16 @@ router.get('', (req, res) => {
     
     .then {
         //  .then response goes here 
-        
+
     }
     // catch errors 
     .catch ((err) => res.status(404).json(err));
 
 });
+
+
+//  DELETE notes function here 
+//DELETE /api/notes/:id
 
 // export router file
 module.exports = router;
